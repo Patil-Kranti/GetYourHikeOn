4ab229d6da067769fc8a289c8278fbd2d70d649a
Kranti Patil <PatilKranti03@gmail.com>
2018-10-24 22:57:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kranti_laptop/getyourhikeon/DataModel.java
@@ -0,0 +1,29 @@
+package com.example.kranti_laptop.getyourhikeon;
+
+public class DataModel {
+
+    String name,location;
+    int id_;
+    String image;
+
+
+    public DataModel(String name,String location, int id_, String image) {
+        this.name = name;
+        this.location = location;
+        this.id_ = id_;
+        this.image=image;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getImage() {
+        return image;
+    }
+    public int getId() {
+        return id_;
+    }
+}
---
+++ app/src/main/java/com/example/kranti_laptop/getyourhikeon/MyData.java
@@ -0,0 +1,15 @@
+package com.example.kranti_laptop.getyourhikeon;
+
+public class MyData {
+
+    static String[] nameArray = {"KUNGSLEDEN","GRAND CANYON HIKE","EVEREST BASE CAMP TREK"};
+
+    static String[] locationArray = {"Sweden","Arizona","Nepal"};
+
+    static Integer[] drawableArray = {R.drawable.slide2,R.drawable.slide3,R.drawable.slide4};
+
+    static Integer[] id_ = {0,1,2};
+
+
+
+}
